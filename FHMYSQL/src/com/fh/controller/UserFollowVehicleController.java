dba7402945eaf799dfef9f22672ba05384ab4b2c
gaoyibing <Abcd@1234>
2016-09-12 18:50:45 +0800

    update

---
+++ FHMYSQL/src/com/fh/controller/UserFollowVehicleController.java
@@ -0,0 +1,8 @@
+package com.fh.controller;
+
+/**
+ * 用户关注车辆
+ */
+public class UserFollowVehicleController {
+
+}
---
+++ FHMYSQL/src/com/fh/service/UserFollowVehicleService.java
@@ -0,0 +1,18 @@
+package com.fh.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("userFollowVehicleService")
+public class UserFollowVehicleService {
+
+    /**
+     * 查询用户关注的车辆
+     */
+
+    /**
+     * 查询用户关注且付费的车辆
+     */
+
+
+
+}
